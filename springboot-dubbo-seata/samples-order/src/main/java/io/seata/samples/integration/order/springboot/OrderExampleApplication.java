a549f33d13fddcd7705aff1f24de67aba791fce2
yuwenbo <yyuweb@outlook.com>
2023-07-26 22:50:59 +0800

    fix:

--- springboot-dubbo-seata/samples-order/src/main/java/io/seata/samples/integration/order/springboot/OrderExampleApplication.java
+++ springboot-dubbo-seata/samples-order/src/main/java/io/seata/samples/integration/order/springboot/OrderExampleApplication.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package io.seata.samples.integration.springboot;
+package io.seata.samples.integration.order.springboot;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
