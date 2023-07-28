c2f1a8f8d34e27508b541fed98638e2ec9905c8e
yuwenbo <yyuweb@outlook.com>
2023-07-28 11:17:57 +0800

    fix:

--- tcc/local-tcc-sample/src/test/java/io/seata/samples/tcc/SeataServerStarter.java
+++ tcc/local-tcc-sample/src/test/java/io/seata/samples/tcc/SeataServerStarter.java
@@ -26,21 +26,21 @@ import io.seata.server.Server;
  */
 public class SeataServerStarter {
 
-    /**
-     * The Server.
-     */
-    static Server server = null;
-
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     * @throws IOException the io exception
-     */
-    public static void main(String[] args) throws IOException {
-        server = new Server();
-        server.main(args);
-
-    }
+//    /**
+//     * The Server.
+//     */
+//    static Server server = null;
+//
+//    /**
+//     * The entry point of application.
+//     *
+//     * @param args the input arguments
+//     * @throws IOException the io exception
+//     */
+//    public static void main(String[] args) throws IOException {
+//        server = new Server();
+//        server.main(args);
+//
+//    }
 
 }
--- tcc/springboot-tcc-sample/src/test/java/io/seata/samples/tcc/provider/SpringbootTccProviderApplication.java
+++ tcc/springboot-tcc-sample/src/test/java/io/seata/samples/tcc/provider/SpringbootTccProviderApplication.java
@@ -30,26 +30,26 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource("classpath:provider/*.xml")
 public class SpringbootTccProviderApplication {
 
-    private static TestingServer server;
-
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     * @throws Exception the exception
-     */
-    public static void main(String[] args) throws Exception {
-        //mock zk server
-        mockZKServer();
-
-        ApplicationContext applicationContext = SpringApplication.run(SpringbootTccProviderApplication.class, args);
-
-    }
-
-    private static void mockZKServer() throws Exception {
-        //Mock zk server，作为  配置中心
-        server = new TestingServer(2181, true);
-        server.start();
-    }
+//    private static TestingServer server;
+//
+//    /**
+//     * The entry point of application.
+//     *
+//     * @param args the input arguments
+//     * @throws Exception the exception
+//     */
+//    public static void main(String[] args) throws Exception {
+//        //mock zk server
+//        mockZKServer();
+//
+//        ApplicationContext applicationContext = SpringApplication.run(SpringbootTccProviderApplication.class, args);
+//
+//    }
+//
+//    private static void mockZKServer() throws Exception {
+//        //Mock zk server，作为  配置中心
+//        server = new TestingServer(2181, true);
+//        server.start();
+//    }
 
 }
