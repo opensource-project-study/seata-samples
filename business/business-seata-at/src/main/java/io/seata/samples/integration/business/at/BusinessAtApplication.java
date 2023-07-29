769d8fc685b6f1360d2d115e208ebf66e3ccb51c
yuwenbo <yyuweb@outlook.com>
2023-07-29 18:42:32 +0800

    fix: 简化client配置

--- business/business-seata-at/src/main/java/io/seata/samples/integration/business/at/BusinessAtApplication.java
+++ business/business-seata-at/src/main/java/io/seata/samples/integration/business/at/BusinessAtApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 事务管理器 TM，用于发起事务
+ */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }, scanBasePackages = {"io.seata.samples.integration.business"})
 public class BusinessAtApplication {
 
